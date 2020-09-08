c18d46f928d91be526bd1c221db43aced488fda6
Leonteva Anastasia <lagarnas@gmail.com>
2020-09-08 11:36:53 +0400

    Add RowLayout class

---
+++ VkNewsFeed/Newsfeed/NewsfeedCellGalleryView/RowLayout.swift
@@ -0,0 +1,13 @@
+//
+//  RowLayout.swift
+//  VkNewsFeed
+//
+//  Created by Анастасия Леонтьева on 08.09.2020.
+//  Copyright © 2020 lagarnas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RowLayout: UICollectionViewLayout {
+}
