bcc7a5cb7c4ba74d9a9ba12536fe58fa0f544917
Leonteva Anastasia <lagarnas@gmail.com>
2020-09-08 12:11:48 +0400

    Add functions in RowLayout Add RowLayoutDelegate

--- VkNewsFeed/Newsfeed/NewsfeedCellGalleryView/NewsfeedCellGalleryCollectionView.swift
+++ VkNewsFeed/Newsfeed/NewsfeedCellGalleryView/NewsfeedCellGalleryCollectionView.swift
@@ -16,7 +16,8 @@ class NewsfeedCellGalleryCollectionView: UICollectionView {
   init() {
     let layout = UICollectionViewFlowLayout()
     layout.scrollDirection = .horizontal
-    super.init(frame: .zero, collectionViewLayout: layout)
+    let rowLayout = RowLayout()
+    super.init(frame: .zero, collectionViewLayout: rowLayout)
     delegate = self
     dataSource = self
     
--- VkNewsFeed/Newsfeed/NewsfeedCellGalleryView/RowLayout.swift
+++ VkNewsFeed/Newsfeed/NewsfeedCellGalleryView/RowLayout.swift
@@ -9,5 +9,62 @@
 import Foundation
 import UIKit
 
+protocol RowLayoutDelegate: class {
+  func collectionView(_ collectionView: UICollectionView, photoAddIndexPath indexPath: IndexPath) -> CGSize
+}
+
 class RowLayout: UICollectionViewLayout {
+  
+  weak var delegate: RowLayoutDelegate!
+  
+  fileprivate var numberOfRows = 1
+  fileprivate var cellPadding: CGFloat = 8
+  
+  fileprivate var cache = [UICollectionViewLayoutAttributes]()
+  
+  fileprivate var contenWidth: CGFloat = 0
+  
+  //константа
+  fileprivate var contentHeight: CGFloat {
+    
+    guard let collectionView = collectionView else { return 0 }
+    
+    let insets = collectionView.contentInset
+    
+    return collectionView.bounds.height - (insets.left + insets.right)
+  }
+  
+  override var collectionViewContentSize: CGSize {
+    return CGSize(width: contenWidth, height: contentHeight)
+  }
+  
+  override func prepare() {
+    guard
+      cache.isEmpty == true, let collectionView = collectionView else { return }
+    
+    var photos = [CGSize]()
+    
+    for item in 0..<collectionView.numberOfItems(inSection: 0) {
+      let indexPath = IndexPath(item: item, section: 0)
+      let photoSize = delegate.collectionView(collectionView, photoAddIndexPath: indexPath)
+      photos.append(photoSize)
+    }
+    
+    let superViewWidth = collectionView.frame.width
+    guard let rowHeight = self.rowHeghtCounter(superViewWidth: superViewWidth, photosArray: photos) else { return }
+  }
+  
+  private func rowHeghtCounter(superViewWidth: CGFloat, photosArray: [CGSize]) -> CGFloat? {
+    var rowHeight: CGFloat
+    
+    let photoWidthMinRatio = photosArray.min {
+      ($0.height / $0.width) < ($1.height / $1.width)
+    }
+    guard let myPhotoWidthMinRatio = photoWidthMinRatio else { return nil}
+    
+    let difference = superViewWidth / myPhotoWidthMinRatio.width
+    rowHeight = myPhotoWidthMinRatio.height * difference
+    
+    return rowHeight
+  }
 }
